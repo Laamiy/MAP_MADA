969a0484d847fa433d9a9af2ba00490981dff27e
nomenaandriamanampisoa <nomenaandriamanampisoa@gmail.com>
2025-12-13 14:06:16 +0300

    vo hanomboka sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { X } from 'lucide-react';
-import { QuickActions } from './QuickActions';
+import { MapPin, X } from 'lucide-react';
 import { SavedPlacesList } from './SavedPlacesList';
 import { RecentPlacesList } from './RecentPlacesList';
 import type { Place, SavedPlace } from '../../types/map.types';
@@ -26,9 +25,11 @@ export const Sidebar: React.FC<SidebarProps> = ({
       <div className={layoutStyles.sidebarContent}>
         <div className="!p-1 border-b ">
           <div className="flex items-center justify-between mb-6">
-            <h2 className="text-xl font-semibold text-gray-800 cursor-pointer">
-              Menu
-            </h2>
+            <div className="flex items-center gap-3">
+              <span className="text-2xl font-semibold text-[#00cc00]">
+                Rapide Maps
+              </span>
+            </div>
             <button
               onClick={onClose}
               className={buttonStyles.icon}
@@ -37,7 +38,6 @@ export const Sidebar: React.FC<SidebarProps> = ({
               <X className="w-5 h-5 text-gray-600" />
             </button>
           </div>
-          <QuickActions />
         </div>
 
         <SavedPlacesList places={savedPlaces} onPlaceClick={onPlaceClick} />
